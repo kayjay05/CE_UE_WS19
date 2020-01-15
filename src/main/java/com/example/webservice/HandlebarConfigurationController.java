90ebd1c72180540d0ab92f7faae06b1b9968b1fa
ElisabethEmp <lilli.e@gmx.at>
2020-01-15 15:12:37 +0100

    Test

--- src/main/java/com/example/webservice/HandlebarConfigurationController.java
+++ src/main/java/com/example/webservice/HandlebarConfigurationController.java
@@ -1,9 +1,12 @@
 package com.example.webservice;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HandlebarConfigurationController {
+    public void TestMethod(@RequestParam(value="test", defaultValue="sample") String name){
 
+    }
 
 }
